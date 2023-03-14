3fd11ea6a58aacb0f5616e5c119287dafc97552b
arochedereux2022 <alexis.rochedereux2022@campus-eni.fr>
2023-03-14 12:00:09 +0100

    oui

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+//    annotation pour ECF
+//    /**
+//     * @Route("/", name="main_home")
+//     */
+    #[Route("/", name: "main_home")]
+    public function home() {
+        //retourne une réponse au navigateur
+        return $this->render("main/home.html.twig");
+    }
+
+    //    annotation pour ECF
+//    /**
+//     * @Route("/test", name="main_test")
+//     */
+    #[Route("/test", name: "main_test")]
+    public function test() {
+        return $this->render("main/test.html.twig");
+    }
+}
